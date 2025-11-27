923d05c9167e112428b533ade75d1d6e46f36162
Claudemyr <claudemyr.medeiros@escolar.ifrn.edu.br>
2025-11-27 16:15:49 -0300

    Ajustes de rotas e serializer

--- backendlojacarros/backendapp/models.py
+++ backendlojacarros/backendapp/models.py
@@ -5,3 +5,5 @@ class Carros(models.Model):
     modelo = models.CharField(max_length=100)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     urlimagem = models.CharField(max_length=255)
+    def __str__(self):
+        return self.modelo
--- backendlojacarros/backendapp/serializers.py
+++ backendlojacarros/backendapp/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Carros
+
 class CarrosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Carros
-        fields = ['modelo', 'preco', 'urlimagem']
+        fields = '__all__'
