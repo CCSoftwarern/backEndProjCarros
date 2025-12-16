1ade60ad238c455ae9f70c6b1513c73dfcb780b1
Claudemyr <claudemyr.medeiros@escolar.ifrn.edu.br>
2025-12-16 18:58:13 -0300

    Alterando a versão do django

--- backendlojacarros/backendlojacarros/settings.py
+++ backendlojacarros/backendlojacarros/settings.py
@@ -77,13 +77,27 @@ WSGI_APPLICATION = 'backendlojacarros.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
+# settings.py
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'carros',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
