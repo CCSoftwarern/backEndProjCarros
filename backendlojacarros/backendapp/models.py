e09efeb46bdc818e0c0119d8ef52b121f9ecd9f7
Claudemyr <claudemyr.medeiros@escolar.ifrn.edu.br>
2025-12-17 20:15:25 -0300

    Correções do conector

--- backendlojacarros/backendapp/models.py
+++ backendlojacarros/backendapp/models.py
@@ -5,6 +5,12 @@ class Carros(models.Model):
     modelo = models.CharField(max_length=100)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     urlimagem = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'carros'  # nome da tabela no banco
+        verbose_name = 'Carro'
+        verbose_name_plural = 'Carros'
+        
     def __str__(self):
         return self.modelo
     
--- backendlojacarros/backendlojacarros/settings.py
+++ backendlojacarros/backendlojacarros/settings.py
@@ -100,16 +100,16 @@ WSGI_APPLICATION = 'backendlojacarros.wsgi.application'
 # }
 
 DATABASES = {
-        'default': {
-            'NAME': 'carros',
-            'ENGINE': 'mysql.connector.django',
-            'USER': 'root',
-            'PASSWORD': '',
-            'OPTIONS': {
-            'autocommit': True,
-            },
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'carros',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
